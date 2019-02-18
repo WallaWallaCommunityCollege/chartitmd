90dc683acab1f0ca797d029ca4d142bd77227d3a
Dragonrun1 <Dragonrun1@gmail.com>
2019-02-18 15:56:36 -0800

    Added new ChartItMD class file to act as entry point to web application.

---
+++ src/ChartItMD.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+/**
+ * Contains class ChartItMD.
+ *
+ * PHP version 7.0+
+ *
+ * LICENSE:
+ * This file is part of ChartItMD.
+ * Copyright (C) 2019 ChartItMD Development Group
+ *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2019 ChartItMD Development Group
+ * @license   Proprietary
+ */
+
+namespace ChartItMD;
+
+use DI\Bridge\Slim\App;
+use DI\ContainerBuilder;
+
+/**
+ * Class ChartItMD.
+ */
+class ChartItMD extends App {
+    /**
+     * @param ContainerBuilder $builder
+     *
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
+     */
+    protected function configureContainer(ContainerBuilder $builder): void {
+        $builder->addDefinitions(dirname(__DIR__) . '/config/config.php');
+        $builder->addDefinitions(dirname(__DIR__) . '/config/pdo.php');
+        $builder->addDefinitions(dirname(__DIR__) . '/config/doctrine.php');
+    }
+}
