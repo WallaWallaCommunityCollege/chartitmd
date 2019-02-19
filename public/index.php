c383f9886001a860eb16de92a55c9d9e03eb8e62
Dragonrun1 <Dragonrun1@gmail.com>
2019-02-18 16:11:03 -0800

    Switch from using Slim/App directly to new ChartItMD wrapper class. Updated index.php to have new env loader stuff. Misc other minor changes.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,10 @@
 <?php
+declare(strict_types=1);
 if ('cli-server' === PHP_SAPI) {
     // To help the built-in PHP dev server, check if the request was actually for
-    // something which should probably be served as a static file
+    // something which should probably be served as a static file.
+    // This closely matches what the rewrite rules in .htaccess file for Apache
+    // does for us.
     $url = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
     if (is_file($file)) {
@@ -11,20 +14,26 @@ if ('cli-server' === PHP_SAPI) {
 $root_path = str_replace('\\', '/', dirname(__DIR__));
 /** @noinspection PhpIncludeInspection */
 require $root_path . '/bootstrap.php';
+
+use ChartItMD\ChartItMD;
+use josegonzalez\Dotenv\Loader;
+
+// Load the environmental variables from .env in the root directory.
+$env = (new Loader([dirname(__DIR__) . '/.env']))->parse();
+$env->toEnv()
+    ->putenv();
 session_start();
-// Instantiate the app
-$settings = require $root_path . '/config/settings.php';
 try {
-    $app = new \Slim\App($settings);
+    $app = new ChartItMD();
 } catch (Exception $e) {
     print $e->getTraceAsString();
 }
 // Set up dependencies
-require $root_path . '/src/dependencies.php';
+//require $root_path . '/src/dependencies.php';
 // Register middleware
-require $root_path . '/src/middleware.php';
+//require $root_path . '/src/middleware.php';
 // Register routes
-require $root_path . '/src/routes.php';
+//require $root_path . '/src/routes.php';
 // Run app
 try {
     $app->run();
