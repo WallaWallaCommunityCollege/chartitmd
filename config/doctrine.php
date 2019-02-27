dda1b23977ccefa34c55decefe2679dc80a00f09
Dragonrun1 <Dragonrun1@gmail.com>
2019-02-27 01:50:53 -0800

    Added new Doctrine Uuid64Type custom datatype class. Updated config/doctrine.php to have parameter for custom datatypes and automatically add them when EntityManager factory is called. Changed EM factory to create own DB connection from PDO parameters instead of trying to re-use existing one. This makes it more flexible.

--- config/doctrine.php
+++ config/doctrine.php
@@ -15,9 +15,10 @@ declare(strict_types=1);
  * @license   Proprietary
  */
 
-use ChartItMD\Pdo\Connection;
+use ChartItMD\Models\Types\Uuid64Type;
 use Doctrine\Common\Cache\ApcuCache;
 use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\Configuration as ORMConfiguration;
 use Doctrine\ORM\EntityManager;
 use Psr\Container\ContainerInterface;
@@ -25,9 +26,12 @@ use function DI\autowire;
 use function DI\string;
 
 $doctrine = [
-    'ChartItMD.Doctrine.entityDir' => string('{ChartItMD.srcDir}Entities/'),
-    'ChartItMD.Doctrine.proxyDir' => string('{ChartItMD.srcDir}Proxies/'),
-    'ChartItMD.Doctrine.Parameters.proxyNamespace' => 'ChartItMD\Proxies',
+    'ChartItMD.Doctrine.entityDir' => string('{ChartItMD.srcDir}Models/Entities/'),
+    'ChartItMD.Doctrine.proxyDir' => string('{ChartItMD.srcDir}Models/Proxies/'),
+    'ChartItMD.Doctrine.Parameters.proxyNamespace' => 'ChartItMD\Models\Proxies',
+    'ChartItMD.Doctrine.Parameters.CustomTypes' => [
+        'uuid64' => Uuid64Type::class
+    ],
     ORMConfiguration::class => autowire(ORMConfiguration::class),
     EntityManager::class => function (ContainerInterface $dic): EntityManager {
         $isDebug = $dic->get('mode') === 'debug';
@@ -35,17 +39,31 @@ $doctrine = [
         $cache = new $cache;
         $config = $dic->get(ORMConfiguration::class);
         $config->setMetadataCacheImpl($cache);
+        $config->setQueryCacheImpl($cache);
+        $config->setResultCacheImpl($cache);
         $driverImpl = $config->newDefaultAnnotationDriver($dic->get('ChartItMD.Doctrine.entityDir'));
         $config->setMetadataDriverImpl($driverImpl);
-        $config->setQueryCacheImpl($cache);
         $config->setProxyDir($dic->get('ChartItMD.Doctrine.proxyDir'));
         $config->setProxyNamespace($dic->get('ChartItMD.Doctrine.Parameters.proxyNamespace'));
         $config->setAutoGenerateProxyClasses($isDebug);
-        $connectionOptions = [
-            'pdo' => $dic->get(Connection::class)
-                         ->getPdo()
+        $customTypes = $dic->get('ChartItMD.Doctrine.Parameters.CustomTypes');
+        if (!empty($customTypes)) {
+            foreach ($customTypes as $type => $class) {
+                Type::addType($type, $class);
+            }
+        }
+        // Assemble database parameters.
+        $connection = [
+            'driver' => 'pdo_' . $dic->get('ChartItMD.Pdo.Parameters.platform'),
+            'host' => $dic->get('ChartItMD.Pdo.Parameters.hostName'),
+            'user' => $dic->get('ChartItMD.Pdo.Parameters.username'),
+            'password' => $dic->get('ChartItMD.Pdo.Parameters.password'),
+            'dbname' => $dic->get('ChartItMD.Pdo.Parameters.database')
         ];
-        return EntityManager::create($connectionOptions, $config);
+        $em = EntityManager::create($connection, $config);
+        $em->getConnection()
+           ->exec((string)$dic->get('ChartItMD.Pdo.Parameters.initialization'));
+        return $em;
     },
 ];
 return $doctrine;
