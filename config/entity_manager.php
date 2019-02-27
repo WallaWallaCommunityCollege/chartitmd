7c53eaeb4e396e6180137629697f12ee6c641c0f
Dragonrun1 <Dragonrun1@gmail.com>
2019-02-27 01:21:26 -0800

    Refactored config/entity_manager.php to use ContainerBuilder and PHP-DI container directly instead of loading all of ChartItMD.

--- config/entity_manager.php
+++ config/entity_manager.php
@@ -15,52 +15,24 @@ declare(strict_types=1);
  */
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-use ChartItMD\ChartItMD;
-use Doctrine\Common\Annotations\AnnotationException;
-use Doctrine\Common\Cache\ArrayCache;
+use DI\ContainerBuilder;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMException;
-use Doctrine\ORM\Tools\Setup;
 use josegonzalez\Dotenv\Loader;
 
 // Initial same environment that application will be running in so they don't
 // become desynchronized later.
+$builder = new ContainerBuilder();
 try {
-    $app = new ChartItMD();
+    $builder->addDefinitions(dirname(__DIR__) . '/config/config.php')
+            ->addDefinitions(dirname(__DIR__) . '/config/dot_env.php')
+            ->addDefinitions(dirname(__DIR__) . '/config/pdo.php')
+            ->addDefinitions(dirname(__DIR__) . '/config/doctrine.php');
+    $dic = $builder->build();
+    $dic->set('mode', 'debug');
+    $dic->get(Loader::class);
+    $entityManager = $dic->get(EntityManager::class);
 } catch (Exception $e) {
     print $e->getTraceAsString();
     exit(1);
-}
-// Get container object.
-$dic = $app->getContainer();
-// Ensure required environment variables are available.
-$dic->get(Loader::class);
-// Assemble database parameters.
-$dbParams = [
-    'driver' => 'pdo_' . $dic->get('ChartItMD.Pdo.Parameters.platform'),
-    'host' => $dic->get('ChartItMD.Pdo.Parameters.hostName'),
-    'user' => $dic->get('ChartItMD.Pdo.Parameters.username'),
-    'password' => $dic->get('ChartItMD.Pdo.Parameters.password'),
-    'dbname' => $dic->get('ChartItMD.Pdo.Parameters.database')
-];
-$cache = new ArrayCache();
-try {
-    $config = Setup::createAnnotationMetadataConfiguration(
-        [$dic->get('ChartItMD.Doctrine.entityDir')],
-        false,
-        $dic->get('ChartItMD.Doctrine.proxyDir'), $cache,
-        false);
-} catch (AnnotationException $e) {
-    print $e->getTraceAsString();
-    exit(2);
-}
-try {
-    $entityManager = EntityManager::create($dbParams, $config);
-} catch (ORMException $e) {
-    print $e->getTraceAsString();
-    exit(3);
-} catch (InvalidArgumentException $e) {
-    print $e->getTraceAsString();
-    exit(4);
 }
 return $entityManager;
