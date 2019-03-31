3f4670d3e1361d333be0ee848ae3d2e575cbf101
Dragonrun1 <Dragonrun1@gmail.com>
2019-03-31 13:56:00 -0700

    Added JsonArrayCollection that extends from Doctrine's ArrayCollection to make json requests easier to process.

---
+++ src/Model/JsonArrayCollection.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+/**
+ * Contains class JsonArrayCollection.
+ *
+ * PHP version 7.2+
+ *
+ * LICENSE:
+ * This file is part of ChartItMD.
+ * Copyright (C) 2019 ChartItMD Development Group
+ *
+ * @author    Michael Cummings <mgcummings@yahoo.com>
+ * @copyright 2019 ChartItMD Development Group
+ * @license   Proprietary
+ */
+
+namespace ChartItMD\Model;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use JsonSerializable;
+
+/**
+ * A quick wrapper to allow JSON encoding of Doctrine Collection.
+ */
+class JsonArrayCollection extends ArrayCollection implements JsonSerializable {
+    /**
+     * Specify data which should be serialized to JSON
+     *
+     * @link  https://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize() {
+        return $this->toArray();
+    }
+}
