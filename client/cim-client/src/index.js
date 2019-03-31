7a37991707a44b23cb7a49bf668fde35da9f6a39
Dragonrun1 <Dragonrun1@gmail.com>
2019-03-31 15:41:07 -0700

    Cleaned up refs to 960 grid from index.html file. Moved patient stuff into new patient.html and js files. Lots of work on patient files to switch from using tables to IMHO cleaner details/summary structure.

--- client/cim-client/src/index.js
+++ client/cim-client/src/index.js
@@ -1,43 +1,5 @@
+'use strict';
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
 const axios = require('axios');
-
-function getPatientAsJson() {
-    // TODO Need to update to use env var here from dotnet for host name.
-    axios.get('https://www.chartitmd.com/~dragonrun1/public/patient/2Y9ovLbO93RUekOOu75TV5')
-        .then(res => {
-            const patient = res.data;
-            patient.age = getAge(patient.dob);
-            patient.bsa = (Math.sqrt(patient.height * patient.weight) / 60).toFixed(1);
-            // noinspection JSSuspiciousNameCombination
-            patient.bmi = (patient.weight / Math.pow(patient.height / 100, 2)).toFixed(1);
-            let order = [
-                'age',
-                'bmi',
-                'bsa',
-                'dob',
-                'firstName',
-                'height',
-                'id',
-                'lastName',
-                'sex',
-                'weight'
-            ];
-            for (let i = 0; i < order.length; i++) {
-                let inp = document.getElementById('patient-' + order[i]);
-                inp.setAttribute('value', patient[order[i]]);
-            }
-        });
-}
-
-getPatientAsJson();
-
-function getAge(dob) {
-    let dt2 = new Date().setHours(12);
-    let dt1 = new Date(dob).setHours(12);
-    let diff = (dt2 - dt1) / 1000;
-    diff /= (60 * 60 * 24);
-    return Math.abs(Math.round(diff / 365.25));
-
-}
---
+++ client/cim-client/src/patient.js
@@ -0,0 +1,97 @@
+'use strict';
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+const axios = require('axios');
+const {DateTime} = require('luxon');
+function getPatientAsJson() {
+    // TODO Need to update to use env var here from dotenv for host name.
+    axios.get('http://localhost/patient/2Y9ovLbO93RUekOOu75TV5')
+         .then(res => {
+             const patient = res.data['patient'];
+             const recentBloodPressures = res.data['recentBloodPressures'];
+             const recentHeights = res.data['recentHeights'];
+             const recentWeights = res.data['recentWeights'];
+             let dob = DateTime.fromSQL(patient['dateOfBirth']['date']);
+             // dob.setHours(12);
+             let age = Math.floor(Math.abs(dob.diffNow('year')
+                                              .as('year')));
+             $("#patient-age")
+                 .text(age);
+             $("#patient-dateOfBirth")
+                 .val(dob.toFormat('yyyy-MM-dd'));
+             $("#patient-firstName")
+                 .val(patient['firstName']);
+             $("#patient-lastName")
+                 .val(patient['lastName']);
+             $("#patient-id")
+                 .val(patient['id']);
+             $("#patient-assignedSex")
+                 .val(patient['gender']['assignedSex']);
+             if (undefined !== recentHeights) {
+                 $("#patient-height")
+                     .val(recentHeights[0]['height']);
+                 let createdAt = DateTime.fromSQL(recentHeights[0]['createdAt']['date']);
+                 $("#patient-height-createdAt")
+                     .val(createdAt.toFormat('yyyy-MM-dd'));
+                 $("#patient-height-id")
+                     .val(recentHeights[0]['id']);
+             } else {
+                 $("#patient-height")
+                     .closest("details")
+                     .hide();
+             }
+             if (undefined === recentWeights) {
+                 $("#patient-weight")
+                     .closest("label")
+                     .hide();
+             } else {
+                 $("#patient-weight")
+                     .val(recentWeights[0]['weight']);
+                 let createdAt = DateTime.fromSQL(recentWeights[0]['createdAt']['date']);
+                 $("#patient-weight-createdAt")
+                     .val(createdAt.toFormat('yyyy-MM-dd'));
+                 $("#patient-weight-id")
+                     .val(recentWeights[0]['id']);
+             }
+             if (undefined === recentHeights || undefined === recentWeights) {
+                 $("#patient-bmi")
+                     .closest("label")
+                     .hide();
+                 $("#patient-bsa")
+                     .closest("label")
+                     .hide();
+             } else {
+                 $("#patient-bmi")
+                     .text(getBMI(recentHeights[0]['height'], recentWeights[0]['weight']));
+                 $("#patient-bsa")
+                     .text(getBSA(recentHeights[0]['height'], recentWeights[0]['weight']));
+             }
+         });
+}
+/**
+ *
+ * @param {number} height
+ * @param {number} weight
+ * @returns {string}
+ */
+function getBMI(height, weight) {
+    return (
+        weight / Math.pow(height / 100, 2)
+    ).toFixed(1);
+}
+/**
+ *
+ * @param {number} height
+ * @param {number} weight
+ * @returns {string}
+ */
+function getBSA(height, weight) {
+    return (
+        Math.sqrt(height * weight) / 60
+    ).toFixed(1);
+}
+$(document)
+    .ready(function () {
+        getPatientAsJson();
+    });
