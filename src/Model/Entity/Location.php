023ca5079916c7ff3d1b895c8d7658fc897831d4
Dragonrun1 <Dragonrun1@gmail.com>
2019-03-31 14:13:07 -0700

    Make Model/Location class into an actual Doctrine Entity. Updated class to use new common trait and implement JsonSerialize interface. Added public getters.

--- src/Model/Entity/Location.php
+++ src/Model/Entity/Location.php
@@ -17,24 +17,86 @@ declare(strict_types=1);
 namespace ChartItMD\Model\Entity;
 
 use ChartItMD\Utils\Uuid4Trait;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Location of measurement, injection site, etc.
  *
  * Class Location.
+ *
+ * @ORM\Table(name="location",
+ *     indexes={
+ *         @ORM\Index(name="idx_created_at", columns={"created_at"})
+ *     },
+ *     uniqueConstraints={
+ *         @ORM\UniqueConstraint(name="uniq_name", columns={"name"})
+ *     }
+ * )
+ * @ORM\Entity(repositoryClass="ChartItMD\Model\Repository\LocationRepository")
  */
 class Location {
     use Uuid4Trait;
+    use EntityCommon;
+    /**
+     * Location constructor.
+     *
+     * @param User   $createdBy
+     * @param string $name
+     *
+     * @throws \Exception
+     */
+    public function __construct(User $createdBy, string $name) {
+        $this->name = $name;
+        $this->id = $this->asBase64();
+        $this->createdAt = new \DateTimeImmutable();
+        $this->createdBy = $createdBy;
+    }
+    /**
+     * @return string
+     */
+    public function getAbbreviation(): string {
+        return $this->abbreviation;
+    }
+    /**
+     * @return string
+     */
+    public function getDescription(): string {
+        return $this->description;
+    }
+    /**
+     * @return string
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+    /**
+     * @param string $value
+     */
+    public function setAbbreviation(string $value): void {
+        $this->abbreviation = $value;
+    }
+    /**
+     * @param string $value
+     */
+    public function setDescription(string $value): void {
+        $this->description = $value;
+    }
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="abbreviation", type="string", length=15, nullable=true)
+     */
     private $abbreviation;
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", nullable=true)
+     */
     private $description;
     /**
      * @var string
      *
-     * @ORM\Column(type="uuid64", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class="ChartItMD\Model\Uuid64Generator")
+     * @ORM\Column(type="string", length=50, nullable=false, unique=true)
      */
-    private $id;
     private $name;
 }
