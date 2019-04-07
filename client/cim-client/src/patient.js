3514fd96aaa05f2abba2edc85ea2813f69f329bb
Dragonrun1 <Dragonrun1@gmail.com>
2019-04-07 10:41:23 -0700

    Updated patient HTML and js files to work with new Patient class and show example for future user configurable view.

--- client/cim-client/src/patient.js
+++ client/cim-client/src/patient.js
@@ -2,97 +2,27 @@
 require('dotenv')
     .config();
 const axios = require('axios');
-const {DateTime} = require('luxon');
-// config axios defaults.
+const Patient = require('./Model/Patient.js');
+// Config Axios defaults.
 axios.defaults.baseURL = process.env.AXIOS_BASE_URL;
 // Setup JQuery
 window.$ = window.jQuery = require('jquery');
+getPatientAsJson();
+
+/**
+ * Simple wrapper around an Axios call to get extended patient detail and display it.
+ */
 function getPatientAsJson() {
     axios.get('/patient/2Y9ovLbO93RUekOOu75TV5')
-         .then(res => {
-             const patient = res.data['patient'];
-             const recentBloodPressures = res.data['recentBloodPressures'];
-             const recentHeights = res.data['recentHeights'];
-             const recentWeights = res.data['recentWeights'];
-             let dob = DateTime.fromSQL(patient['dateOfBirth']['date']);
-             let age = Math.floor(Math.abs(dob.diffNow('year')
-                                              .as('year')));
-             $("#patient-age")
-                 .text(age);
-             $("#patient-dateOfBirth")
-                 .val(dob.toFormat('yyyy-MM-dd'));
-             $("#patient-firstName")
-                 .val(patient['firstName']);
-             $("#patient-lastName")
-                 .val(patient['lastName']);
-             $("#patient-id")
-                 .val(patient['id']);
-             $("#patient-assignedSex")
-                 .val(patient['gender']['assignedSex']);
-             if (undefined !== recentHeights) {
-                 $("#patient-height")
-                     .val(recentHeights[0]['height']);
-                 let createdAt = DateTime.fromSQL(recentHeights[0]['createdAt']['date']);
-                 $("#patient-height-createdAt")
-                     .val(createdAt.toFormat('yyyy-MM-dd'));
-                 $("#patient-height-id")
-                     .val(recentHeights[0]['id']);
-             } else {
-                 $("#patient-height")
-                     .closest("details")
-                     .hide();
-             }
-             if (undefined === recentWeights) {
-                 $("#patient-weight")
-                     .closest("label")
-                     .hide();
-             } else {
-                 $("#patient-weight")
-                     .val(recentWeights[0]['weight']);
-                 let createdAt = DateTime.fromSQL(recentWeights[0]['createdAt']['date']);
-                 $("#patient-weight-createdAt")
-                     .val(createdAt.toFormat('yyyy-MM-dd'));
-                 $("#patient-weight-id")
-                     .val(recentWeights[0]['id']);
-             }
-             if (undefined === recentHeights || undefined === recentWeights) {
-                 $("#patient-bmi")
-                     .closest("label")
-                     .hide();
-                 $("#patient-bsa")
-                     .closest("label")
-                     .hide();
-             } else {
-                 $("#patient-bmi")
-                     .text(getBMI(recentHeights[0]['height'], recentWeights[0]['weight']));
-                 $("#patient-bsa")
-                     .text(getBSA(recentHeights[0]['height'], recentWeights[0]['weight']));
-             }
+         .then(response => {
+             $(document)
+                 .ready(() => {
+                     (
+                         new Patient(response.data)
+                     ).displayDetails(['recentBloodPressures', 'recentHeights', 'recentWeights']);
+                 });
+         })
+         .catch(function (error) {
+             console.log(error);
          });
 }
-/**
- *
- * @param {number} height
- * @param {number} weight
- * @returns {string}
- */
-function getBMI(height, weight) {
-    return (
-        weight / Math.pow(height / 100, 2)
-    ).toFixed(1);
-}
-/**
- *
- * @param {number} height
- * @param {number} weight
- * @returns {string}
- */
-function getBSA(height, weight) {
-    return (
-        Math.sqrt(height * weight) / 60
-    ).toFixed(1);
-}
-$(document)
-    .ready(function () {
-        getPatientAsJson();
-    });
