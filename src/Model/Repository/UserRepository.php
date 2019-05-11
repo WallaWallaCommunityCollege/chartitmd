a5bdab86e887d00a09b3586dee46185acc8599db
Michael Cummings <dragonrun1@gmail.com>
2019-05-11 10:36:55 -0700

    Updated to couple of the *Repository classes.

--- src/Model/Repository/UserRepository.php
+++ src/Model/Repository/UserRepository.php
@@ -16,7 +16,9 @@ declare(strict_types=1);
 
 namespace ChartItMD\Model\Repository;
 
+use ChartItMD\Model\Entity\User;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\ORMException;
 
 /**
  * Class UserRepository.
@@ -31,20 +33,19 @@ class UserRepository extends EntityRepository {
      */
     public function getUserIdByName(string $name): ?string {
         $qb = $this->createQueryBuilder('user');
-        $result =
-            $qb->select('user.id')
-               ->where(
-                   $qb->expr()
-                      ->eq(
-                          'user.name',
-                          $qb->expr()
-                             ->literal($name)
-                      )
-               )
-               ->setMaxResults(1)
-               ->getQuery()
-               ->getArrayResult();
-        return 0 < count($result) ? $result[0]['id'] : null;
+        $result = $qb->select('user.id')
+                     ->where(
+                         $qb->expr()
+                            ->eq(
+                                'user.name',
+                                $qb->expr()
+                                   ->literal($name)
+                            )
+                     )
+                     ->setMaxResults(1)
+                     ->getQuery()
+                     ->getArrayResult();
+        return 0 < \count($result) ? $result[0]['id'] : null;
     }
     /**
      * @return array
@@ -57,4 +58,73 @@ class UserRepository extends EntityRepository {
                   ->getQuery()
                   ->getArrayResult();
     }
+    /**
+     * @param string $id
+     */
+    public function getUserById(string $id) {
+        $result = null;
+        $query = $this->createQueryBuilder('u')
+                      ->select('u')
+                      ->join('p.gender', 'g')
+                      ->join('p.createdBy', 'u')
+                      ->where('p.id = :id')
+                      ->setParameter('id', $id)
+                      ->getQuery();
+    }
+    /**
+     * @param array $data
+     *
+     * @return User|null
+     */
+    public function userLogin(array $data): ?User {
+        $result = null;
+        $query = $this->createQueryBuilder('u')
+                      ->select('u')
+                      ->where('u.name = :name')
+                      ->setParameter('name', $data['name'])
+                      ->getQuery();
+        try {
+            /**
+             * @var User $user
+             */
+            $user = $query->getSingleResult();
+        } catch (\Throwable $e) {
+            var_dump($e->getMessage());
+            return null;
+        }
+        $hash = $user->getPassword();
+        $options = $this->hashOptions ?? null;
+        // Verify stored hash against plain-text password
+        if (\password_verify($data['password'], $hash)) {
+            // Check if a newer hashing algorithm is available
+            // or the cost has changed
+            if (\password_needs_rehash($hash, \PASSWORD_DEFAULT, $options)) {
+                // If so, create a new hash, and replace the old one
+                $user->setPassword(\password_hash($data['password'], \PASSWORD_DEFAULT, $options));
+                try {
+                    $this->getEntityManager()
+                         ->persist($user);
+                } catch (ORMException $e) {
+                    var_dump($e->getMessage());
+                    return null;
+                }
+            }
+            $result = $user;
+        }
+        return $result;
+    }
+    /**
+     * @param User $user
+     */
+    public function addUser(User $user) {
+        // TODO
+    }
+    /**
+     * Per hash algorithm options.
+     *
+     * NOTE: Will need to be update if the default algorithm changes. Assumes PASSWORD_BCRYPT
+     *
+     * @var array
+     */
+    private $hashOptions = ['cost' => 12];
 }
