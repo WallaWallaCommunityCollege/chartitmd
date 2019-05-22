6fa421acb6548a45925c8f2d0f7f56cfaafa6784
Michael Cummings <dragonrun1@gmail.com>
2019-05-22 11:20:02 -0700

    Added cim-client/src/Model/VitalSigns.js class.

--- client/cim-client/src/Model/VitalSigns.js
+++ client/cim-client/src/Model/VitalSigns.js
@@ -2,8 +2,48 @@
 const JsonDate = require('./JsonDate.js');
 window.$ = window.jQuery = require('jquery');
 
-class VitalSigns{
-    constructor(data){
+/**
+ *
+ */
+class VitalSigns {
+    /**
+     *
+     * @param data
+     */
+    constructor(data) {
+        /**
+         * @type {object} data The raw JSON object as from Axios response.
+         */
         this.data = data;
     }
+
+    /**
+     *
+     * @param options
+     */
+    displayDetails(options = []) {
+        $('#patient-vitalSign-diastolic')
+            .text(this.data['diastolic']);
+        $('#patient-vitalSign-systolic')
+            .text(this.data['systolic']);
+        $('#patient-vitalSign-pulseRate')
+            .text(this.data['pulseRate']);
+        $('#patient-vitalSign-respirationRate')
+            .text(this.data['respirationRate']);
+        $('#patient-vitalSign-temperature')
+            .text(this.data['temperature']);
+        $('#patient-vitalSign-temperatureMethod')
+            .text(this.data['temperatureMethod']['name']);
+        $('#patient-vitalSign-pain')
+            .text(this.data['pain']);
+        $('#patient-vitalSign-painLocation')
+            .text(this.data['painLocation']['name']);
+        let createdAt = JsonDate.fromJsonPHPDate(this.data['createdAt']);
+        $('#patient-vitalSign-createdAt')
+            .val(createdAt.toFormat('yyyy-MM-dd'));
+        $('#patient-vitalSign-id')
+            .val(this.data['id']);
+    }
 }
+
+module.exports = VitalSigns;
