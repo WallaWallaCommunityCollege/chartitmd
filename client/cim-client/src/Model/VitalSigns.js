1decc9253be8ae86ec8826729f14c2a627f66f83
Michael Cummings <dragonrun1@gmail.com>
2019-06-09 11:19:48 -0700

    Total re-write of VitalView js class.

--- client/cim-client/src/Model/VitalSigns.js
+++ client/cim-client/src/Model/VitalSigns.js
@@ -1,26 +1,110 @@
 'use strict';
-const JsonDate = require('./JsonDate');
-const DisplayTable = require('./DisplayTable');
+const Measurement = require('./Measurement');
+const ModelCommon = require('./ModelCommon');
+const Patient = require('./Patient');
 window.$ = window.jQuery = require('jquery');
+/**
+ * @typedef {Object} JsonVitals
+ * @property {Object} createdAt
+ * @property {Object} createdBy
+ * @property {string} id
+ * @property {Object} patient
+ * @property {Object} diastolic
+ * @property {Object} systolic
+ * @property {?Object} pulse
+ * @property {?Object} respiration
+ * @property {?Object} oxygenSaturation
+ * @property {?Object} temperature
+ * @property {?Object} pain
+ */
 
+/**
+ * @typedef {Object} Vitals
+ * @property {Measurement} diastolic
+ * @property {Measurement} systolic
+ * @property {?Measurement} pulse
+ * @property {?Measurement} respiration
+ * @property {?Measurement} oxygenSaturation
+ * @property {?Measurement} temperature
+ * @property {?Measurement} pain
+ */
 /**
  *
  */
-class VitalSigns {
+class VitalSigns extends ModelCommon {
     /**
      *
-     * @param data
+     * @param {?Patient} patient
      */
-    constructor(data) {
+    constructor(patient = null) {
+        super();
+        this.displayItems = ['systolic', 'diastolic', 'pulse', 'respiration', 'oxygenSaturation', 'temperature', 'pain'];
         /**
-         * @type {object} data The raw JSON object as from Axios response.
+         *
+         * @type {string}
          */
-        this.data = data;
-        this.displayItems = ['systolic', 'diastolic', 'pulseRate', 'respirationRate', 'temperature', 'pain'];
         this.idBase = '#patient-vitalSigns-';
+        /**
+         *
+         * @type {?Vitals}
+         */
+        this.vitals = null;
+        /**
+         *
+         * @type {?Patient}
+         */
+        this.patient = patient;
+    }
+    /**
+     *
+     * @param {JsonVitals} data
+     * @param {?Patient} patient
+     *
+     * @return VitalSigns
+     */
+    static fromJson(data, patient) {
+        let result = new VitalSigns(patient);
+        Object.keys(data)
+              .forEach(function (key) {
+                  if (null === data[key]) {
+                      return;
+                  }
+                  switch (key) {
+                      case 'createdAt':
+                          result.setCreatedAt(data[key]);
+                          break;
+                      case 'createdBy':
+                          result.setCreatedBy(data[key]);
+                          break;
+                      case 'id':
+                          result.setId(data[key]);
+                          break;
+                      case 'patient':
+                          if (null === result.patient) {
+                              result.patient = Patient.fromJson(data[key]);
+                          }
+                          break;
+                      default:
+                          // Only hydrate things that are going to be displayed.
+                          if (0 >= result.displayItems.indexOf(key)) {
+                              result.addMeasurement(key, data[key]);
+                          }
+                  }
+              });
+        return result;
+    }
+    /**
+     *
+     * @param {string} name
+     * @param {Object} value
+     * @returns {VitalSigns}
+     */
+    addMeasurement(name, value) {
+        this.vitals[name] = Measurement.fromJson(value);
+        return this;
     }
     displayDetails() {
-        (new DisplayTable(this.data, this.idBase, this.displayItems)).displayTable();
+        //TODO
     }
 }
 
